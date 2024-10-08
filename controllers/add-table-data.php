57b69cf80d7fef84fa17272f2275db2fce86d3a1
Kamal Lachinov <kamallachinov5@gmail.com>
2024-10-08 13:09:40 +0400

    removed comments

--- controllers/add-table-data.php
+++ controllers/add-table-data.php
@@ -1,7 +1,6 @@
 <?php
 require "../db/db-connection.php";
 
-// Initialize variables to hold form data and errors
 $imageUrl = '';
 $title = '';
 $desc = '';
@@ -13,12 +12,10 @@ $errors = [
 ];
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Get the form data
     $imageUrl = $_POST['imageUrl'] ?? '';
     $title = $_POST['title'] ?? '';
     $desc = $_POST['description'] ?? '';
 
-    // Validate form data
     if (empty($imageUrl)) {
         $errors['imageUrl'] = "Image URL is required";
     }
@@ -29,7 +26,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $errors['desc'] = "Description is required";
     }
 
-    // If no errors, insert data into the database
     if (empty($errors['imageUrl']) && empty($errors['title']) && empty($errors['desc'])) {
         $stmt = $conn->prepare("INSERT INTO dashboard_data (imageUrl, title, description) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $imageUrl, $title, $desc);
@@ -39,7 +35,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $title = '';
             $desc = '';
         } else {
-            // Handle database error
             $dbError = "Something went wrong. Please try again later.";
         }
 
