7cd163260ebf58e3c1b7d5b4503c9354c321a344
kamallachinov <kamallachinov5@gmail.com>
2024-12-31 15:42:36 +0400

    changes

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php
 
 define('APP_NAME', dirname(__FILE__));
-header(APP_NAME .  "Location: /views/home.view.php");
 require(APP_NAME . "/views/home.view.php");
-
-
-// $isAuthenticated = $_SESSION['isAuthenticated'] ?? false;
-
-// if ($isAuthenticated) {
-//     header(APP_NAME .  "Location: /views/auth/login.view.php");
-//     require(APP_NAME . "/views/auth/login.view.php");
-// } else {
-//     header(APP_NAME .  "Location: /views/home.view.php");
-//     require APP_NAME . "/views/home.view.php";
-// }
+header(APP_NAME .  "Location: /views/home.view.php");
